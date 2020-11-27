2ee9d2aff3a97eeb471ea6e06cb0cddca5caa365
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-27 14:47:08 +0100

    update url mapping

--- reservation/urls.py
+++ reservation/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
         path('hotels', views.list_hotels),
-        path('customers', views.list_customers)
+        path('customers', views.list_customers),
+                path('', views.list_reservation)
+
 
 ]
