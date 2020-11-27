c8885bb2dc69d55cd70fb40c6b1109a765009ce3
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-27 12:09:50 +0100

    add models to admin panel and create superuser

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import *
+
+
+admin.site.register(Customer)
+admin.site.register(Hotel)
+admin.site.register(Reservation)
