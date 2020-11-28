42ee70f5aeaf0009081516a1720583025844ffdb
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-28 14:22:17 +0100

    create another folder called angular in static js

---
+++ static/js/angular/myangapp.js
@@ -0,0 +1,28 @@
+{% block scripts %}
+  
+
+<script>
+
+   (function() {
+      angular.module("hotelreservation.angapp",[]).
+      controller("HotelReservation", ["$scope",HotelReservation]) ;
+
+
+      function HotelReservation($scope) {
+
+         $scope.hotels = [
+         {hotel_name : "Retz Carlton", hotel_city : "Casa"},
+         {hotel_name : "Sheraton", hotel_city:"Rabat"},
+         ];
+         $scope.add_new_hotel = function(new_hotel,hotels) {
+
+            var hotel = { hotel_name : new_hotel, hotel_city :"Any city"} ;
+            hotels.push(hotel) ;
+
+         } ;
+      }
+
+   })();
+
+</script>
+{% endblock scripts  %}
