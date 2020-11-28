0d6fa054a3156c00dcc9778057d31e06af12c0dc
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-28 15:56:35 +0100

    add REST app to installed apps in setting file

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservation'
+    'reservation',
+        'rest_framework',
 ]
 
 MIDDLEWARE = [
