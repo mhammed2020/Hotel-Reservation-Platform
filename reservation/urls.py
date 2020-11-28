f02bd72e31442702d495a22bd4a29f37ed6ed297
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-28 16:50:51 +0100

    add django router to our api

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
 from . import api
-urlpatterns = [
+from django.conf.urls import url
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("hotelsapi",api.HotelAPI)
+urlpatterns = router.urls
+urlpatterns += [
+
         path('hotels', views.list_hotels),
-                path('hotelsapi', api.HotelAPI.as_view()),
+                # path('hotelsapi', api.HotelAPI.as_view()),
 
         path('customers', views.list_customers),
                 path('', views.list_reservation)
