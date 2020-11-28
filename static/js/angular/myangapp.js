eaf51ee9cc548f740c274276c947dbd396746c46
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-28 16:33:54 +0100

    delete static json from controller and add django rest API

--- static/js/angular/myangapp.js
+++ static/js/angular/myangapp.js
@@ -3,16 +3,13 @@
 
    (function() {
       angular.module("hotelreservation.angapp",[]).
-      controller("HotelReservation", ["$scope",HotelReservation])
+      controller("HotelReservation", ["$scope","$http", HotelReservation])
       .controller("CustomersController",["$scope",CustomersController]) ;
 
 
     function HotelReservation($scope) {
 
-         $scope.hotels = [
-         {hotel_name : "Retz Carlton", hotel_city : "Casa"},
-         {hotel_name : "Sheraton", hotel_city:"Rabat"},
-         ];
+         $scope.hotels = [];
          $scope.add_new_hotel = function(new_hotel,hotels) {
 
             var hotel = { hotel_name : new_hotel, hotel_city :"Any city"} ;
