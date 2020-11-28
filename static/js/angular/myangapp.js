434c7cfa8b32a188a52e6c34b9edd53354f53e8e
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-28 16:39:37 +0100

    get data from django  to angular

--- static/js/angular/myangapp.js
+++ static/js/angular/myangapp.js
@@ -7,9 +7,15 @@
       .controller("CustomersController",["$scope",CustomersController]) ;
 
 
-    function HotelReservation($scope) {
+    function HotelReservation($scope,$http) {
 
          $scope.hotels = [];
+
+        $http.get("/reservation/hotelsapi")
+        .then(function(response) {
+            $scope.hotels = response.data;
+        }) ;
+
          $scope.add_new_hotel = function(new_hotel,hotels) {
 
             var hotel = { hotel_name : new_hotel, hotel_city :"Any city"} ;
