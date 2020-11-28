1b5b5ce201f2cff64cd23ea1eede8373abecb8f4
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-28 16:22:03 +0100

    Add hoels view

--- reservation/urls.py
+++ reservation/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 from . import api
 urlpatterns = [
-        # path('hotels', views.list_hotels),
-                path('hotels', api.HotelAPI.as_view()),
+        path('hotels', views.list_hotels),
+                path('hotelsapi', api.HotelAPI.as_view()),
 
         path('customers', views.list_customers),
                 path('', views.list_reservation)
